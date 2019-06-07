b9ff7cc11e0b10005242853472579f5617014f01
Raphaël Desalbres <raphael.desalbres@outlook.com>
2019-06-07 22:46:33 +0200

    initial commit
    initial commit

---
+++ ShoppingCart.Core/Models/Items.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingCart.Core.Models
+{
+    public partial class Items
+    {
+        public Items()
+        {
+            Carts = new HashSet<Carts>();
+            OrderDetails = new HashSet<OrderDetails>();
+        }
+
+        public int Id { get; set; }
+        public int CatagorieId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ItemPictureUrl { get; set; }
+        public byte[] InternalImage { get; set; }
+
+        public Catagories Catagorie { get; set; }
+        public ICollection<Carts> Carts { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
