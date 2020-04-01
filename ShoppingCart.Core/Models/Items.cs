04f9dfb772dd1146e7c19613c43908a19c778d14
Raphaël Desalbres <raphael.desalbres@outlook.com>
2020-04-01 13:45:08 +0200

    Update Items.cs

--- ShoppingCart.Core/Models/Items.cs
+++ ShoppingCart.Core/Models/Items.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace ShoppingCart.Core.Models
@@ -12,12 +12,11 @@ namespace ShoppingCart.Core.Models
         }
 
         public int Id { get; set; }
-        public int CategoryId { get; set; }
+        public int CatagorieId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string ItemPictureUrl { get; set; }
         public byte[] InternalImage { get; set; }
-
         public Categories Category { get; set; }
         public ICollection<Carts> Carts { get; set; }
         public ICollection<OrderDetails> OrderDetails { get; set; }
