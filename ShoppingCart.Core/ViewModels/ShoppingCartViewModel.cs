d80e9d8dfc31aea9359d4b87a3a1ab9036e39528
Raphaël Desalbres <raphael.desalbres@outlook.com>
2020-04-01 13:47:50 +0200

    Update ShoppingCartViewModel.cs

--- ShoppingCart.Core/ViewModels/ShoppingCartViewModel.cs
+++ ShoppingCart.Core/ViewModels/ShoppingCartViewModel.cs
@@ -1,6 +1,7 @@
-﻿using ShoppingCart.Core.Models;
+using ShoppingCart.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ShoppingCart.Core.ViewModels
